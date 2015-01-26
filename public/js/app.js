898019742cb00087b3defeb1abe0a9a4a9c63f3b
stephjin <stephanie.k.jin@gmail.com>
2015-01-26 12:04:10 -0800

    Add three views.

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+angular.module('routerApp', ['routerRoutes'])
+
+.controller('mainController', function(){
+  var vm = this;
+  vm.bigMessage = 'This is some big header text';
+})
+
+.controller('homeController', function(){
+  var vm = this;
+  vm.message = 'This is the home page';
+})
+
+.controller('aboutController', function(){
+  var vm = this;
+  vm.message = 'This is the about page';
+})
+
+.controller('contactController', function(){
+  var vm = this;
+  vm.message = 'This is the contact page';
+});
