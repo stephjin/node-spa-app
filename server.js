17b068f29eb2698a044a076acfd0a5c01565faf1
stephjin <stephanie.k.jin@gmail.com>
2015-01-26 11:27:45 -0800

    Set up basic index.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 
 // set up single route to index.html
 app.get('*', function(req,res){
-  res.sendfile(path.join(__dirname + '/public/views/index.html'));
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 // server
